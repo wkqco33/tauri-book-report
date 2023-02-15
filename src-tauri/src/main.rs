3d07c689eabf4750a586b5c98fffaf328ce3d06e
wkqco33 <wkqco33@gmail.com>
2023-02-15 23:33:46 +0900

    Updated main page

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+mod controller;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
